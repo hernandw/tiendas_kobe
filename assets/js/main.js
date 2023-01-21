67fc0863935cb0e1d113d3d29d0d2f5a6c2c1b3a
Williams Hernandez <hernandw@gmail.com>
2023-01-21 13:35:35 -0300

    :sparkles: menu mobile and menu cart actives

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,32 @@
-const menuEmail = document.querySelector('.nabvar-email').addEventListener('click', toggleDesktopMenu)
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuEmail = document
+  .querySelector(".nabvar-email")
+  .addEventListener("click", toggleMenuMobile);
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamburguesa = document
+  .querySelector(".menu")
+  .addEventListener("click", toggleMenuHamburguer);
+const menuMobile = document.querySelector(".menu-mobile");
+const menuCart = document
+  .querySelector(".navbar-shopping-cart")
+  .addEventListener("click", toggleMenuProductCart);
+const asideProduct = document.querySelector(".my-order");
 
+function toggleMenuMobile() {
+  desktopMenu.classList.toggle("inactive");
+}
 
-function toggleDesktopMenu(){
-desktopMenu.classList.toggle('inactive')
+function toggleMenuHamburguer() {
+  const isMenuCartOpen = !asideProduct.classList.contains("inactive");
+  if (isMenuCartOpen) {
+    asideProduct.classList.add("inactive");
+  }
+  menuMobile.classList.toggle("inactive");
+}
+
+function toggleMenuProductCart() {
+  const isMenuMobileOpen = !menuMobile.classList.contains("inactive");
+  if (isMenuMobileOpen) {
+    menuMobile.classList.add("inactive");
+  }
+  asideProduct.classList.toggle("inactive");
 }
