cb4de7c935c0b1edded1dc22e62ef7ceab40ed5d
Williams Hernandez <hernandw@gmail.com>
2023-01-22 14:25:29 -0300

    :tada: add page product details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,76 @@
+const productsList = [
+    {
+      id: 1,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/295560932_778979743233538_185955359791984085_n.webp",
+    },
+    {
+      id: 2,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/298704855_1221544621979887_3183945203523051853_n.webp",
+    },
+    {
+      id: 3,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/298547308_1120556871895580_1501991367355774541_n.webp",
+    },
+    {
+      id: 4,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/299327229_387915529943851_5366369589836518095_n.webp",
+    },
+    {
+      id: 5,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/299122303_1130143204524741_6154636218172177359_n.webp",
+    },
+    {
+      id: 6,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/298794152_105042122270224_6286121474671587556_n.webp",
+    },
+    {
+      id: 7,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/298753883_125482326890611_7872018069092673500_n.webp",
+    },
+    {
+      id: 8,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/309414520_1297438217750833_4726051445296664449_n.webp",
+    },
+    {
+      id: 9,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/309431477_415561680659706_2258555334824791960_n.webp",
+    },
+    {
+      id: 10,
+      name: "Blusa X",
+      price: "$10",
+      image:
+        "./assets/images/blusas/298688930_755782142201716_1424929170547578041_n.webp",
+    },
+  ];
+
 const menuEmail = document
   .querySelector(".nabvar-email")
   .addEventListener("click", toggleMenuMobile);
@@ -10,6 +83,13 @@ const menuCart = document
   .querySelector(".navbar-shopping-cart")
   .addEventListener("click", toggleMenuProductCart);
 const asideProduct = document.querySelector(".my-order");
+
+
+
+
+/* btnDetails.addEventListener('click', ()=>{
+    console.log('hola')
+}) */
 
 function toggleMenuMobile() {
   desktopMenu.classList.toggle("inactive");
@@ -31,68 +111,7 @@ function toggleMenuProductCart() {
   asideProduct.classList.toggle("inactive");
 }
 
-const productsList = [
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/295560932_778979743233538_185955359791984085_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/298704855_1221544621979887_3183945203523051853_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/298547308_1120556871895580_1501991367355774541_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/299327229_387915529943851_5366369589836518095_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/299122303_1130143204524741_6154636218172177359_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/298794152_105042122270224_6286121474671587556_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/298753883_125482326890611_7872018069092673500_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/309414520_1297438217750833_4726051445296664449_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/309431477_415561680659706_2258555334824791960_n.webp",
-  },
-  {
-    name: "Blusa X",
-    price: "$10",
-    image:
-      "./assets/images/blusas/298688930_755782142201716_1424929170547578041_n.webp",
-  },
-];
+
 
 const cardProduct = document.querySelector(".card__container");
 function renderProduct() {
@@ -111,13 +130,29 @@ function renderProduct() {
                   <p class="product__name">${product.name}</p>
                 </div>
                 <figure>
-                  <img src="./assets/icons/bt_add_to_cart.svg" alt="" />
+                  <img class="btn" id=id-${product.id} src="./assets/icons/bt_add_to_cart.svg" alt=${product.name} onclick="pulsar(${product.id})" />
                 </figure>
               </div>
             </div>
         `;
   });
+
   cardProduct.innerHTML = template;
 }
 
+
+
 renderProduct();
+
+const order = document.querySelector('.my-order')
+const detailsProduct = document.getElementById('productDetail')
+const productDetailClosed = document.querySelector('.product-details-close').addEventListener('click', detailProductClose)
+
+
+function detailProductClose(){
+    detailsProduct.classList.add('inactive')
+}
+
+function pulsar(id){
+    detailsProduct.classList.remove('inactive')
+}
